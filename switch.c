9c768dc913a400c66c76b3673671b9b01857baf0
meet kathiriya <123069223+Meetu7@users.noreply.github.com>
2023-03-04 12:32:57 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,47 @@
+/*WAP of Addition, Subtraction, Multiplication and Division using Switch
+case.(Must Be Menu Driven)*/
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    int choice;
+
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+
+    printf("Choose an operation to perform:\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+            printf("The sum of %.2f and %.2f is %.2f\n", num1, num2, result);
+            break;
+        case 2:
+            result = num1 - num2;
+            printf("The difference between %.2f and %.2f is %.2f\n", num1, num2, result);
+            break;
+        case 3:
+            result = num1 * num2;
+            printf("The product of %.2f and %.2f is %.2f\n", num1, num2, result);
+            break;
+        case 4:
+            if (num2 == 0) {
+                printf("Error: division by zero\n");
+            } else {
+                result = num1 / num2;
+                printf("The quotient of %.2f and %.2f is %.2f\n", num1, num2, result);
+            }
+            break;
+        default:
+            printf("Error: invalid choice\n");
+    }
+
+    return 0;
+}
